4248091975c202e40693e78345a5e3528d8e3430
Buck, Sebastian <sebastian.buck@adesso.de>
2024-04-12 21:46:59 +0200

    fix dockerbuild

--- next.config.js
+++ next.config.js
@@ -5,6 +5,15 @@
 await import("./src/env.js");
 
 /** @type {import("next").NextConfig} */
-const config = {};
+const config = {
+  output: "standalone",
+
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+};
 
 export default config;
