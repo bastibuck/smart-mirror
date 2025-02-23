b956d582ba84fbc2ddb6aa664073df9ffe987d1a
Buck, Sebastian <sebastian.buck@adesso.de>
2025-02-23 17:36:36 +0100

    add go server with hot-reloading

---
+++ server/go.mod
@@ -0,0 +1,5 @@
+module smartmirror.server
+
+go 1.23.4
+
+require github.com/go-chi/chi/v5 v5.2.1 // indirect
---
+++ server/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+)
+
+func main() {
+	fmt.Println("Starting the application...")
+
+	router := chi.NewRouter()
+
+	router.Get("/", func(res http.ResponseWriter, req *http.Request) {
+		res.Write([]byte("Hello, World!"))
+	})
+
+	// Start the server
+	http.ListenAndServe(":8080", router)
+}
