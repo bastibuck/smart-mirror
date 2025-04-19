20556d42e3acf3ee39d744de652f5f021e39274c
Basti Buck <mail@bastibuck.de>
2025-04-19 15:11:47 +0200

    use prettier for formatting

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,14 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     TanStackRouterVite({
-      target: 'react',
+      target: "react",
       autoCodeSplitting: true,
     }),
     react(),
   ],
-})
+});
