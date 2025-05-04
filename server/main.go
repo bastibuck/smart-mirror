1441e77e64ccd4fb58c0ef1ba6eda3f849dbeac4
Basti Buck <mail@bastibuck.de>
2025-05-04 20:24:15 +0200

    add strava annual stats endpoint

--- server/main.go
+++ server/main.go
@@ -1,11 +1,35 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
 )
+
+// Define a struct for the sports data
+type SportStats struct {
+	Count      int `json:"count"`
+	MovingTime int `json:"moving_time"`
+	Distance   int `json:"distance"`
+}
+
+// Constructor function for SportStats
+func NewSportStats(count, movingTime, distance int) SportStats {
+	return SportStats{
+		Count:      count,
+		MovingTime: movingTime,
+		Distance:   distance,
+	}
+}
+
+// Define a struct for the overall data
+type StravaStats struct {
+	Running SportStats `json:"running"`
+	Cycling SportStats `json:"cycling"`
+	Kiting  SportStats `json:"kiting"`
+}
 
 func main() {
 	fmt.Println("Starting the application...")
@@ -21,6 +45,21 @@ func setupRouter() *chi.Mux {
 
 	router.Get("/", func(res http.ResponseWriter, req *http.Request) {
 		res.Write([]byte("Hello World!"))
+	})
+
+	router.Get("/strava-stats", func(res http.ResponseWriter, req *http.Request) {
+		data := StravaStats{
+			Running: NewSportStats(10, 200, 186),
+			Cycling: NewSportStats(20, 300, 305),
+			Kiting:  NewSportStats(30, 400, 237),
+		}
+
+		// Set the response header to JSON
+		res.Header().Set("Content-Type", "application/json")
+
+		if err := json.NewEncoder(res).Encode(data); err != nil {
+			http.Error(res, "Failed to encode JSON", http.StatusInternalServerError)
+		}
 	})
 
 	return router
