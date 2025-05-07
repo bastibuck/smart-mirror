57cd0bad68c9fa4562493e7bc077d670ae3c9257
Basti Buck <mail@bastibuck.de>
2025-05-08 00:02:18 +0200

    add routes module

---
+++ server/routes/router.go
@@ -0,0 +1,143 @@
+package routes
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/cors"
+	"github.com/patrickmn/go-cache"
+	"smartmirror.server/config"
+)
+
+// Define a struct for the sports data
+type SportStats struct {
+	Count       int `json:"count"`
+	MovingTimeS int `json:"moving_time_s"`
+	Distance    int `json:"distance"`
+}
+
+// Constructor function for SportStats
+func NewSportStats(count, movingTimeS, distance int) SportStats {
+	return SportStats{
+		Count:       count,
+		MovingTimeS: movingTimeS,
+		Distance:    distance,
+	}
+}
+
+// Define a struct for the overall data
+type StravaStats struct {
+	Running SportStats `json:"running"`
+	Cycling SportStats `json:"cycling"`
+}
+
+var stravaCache = cache.New(30*time.Minute, 45*time.Hour)
+
+func SetupRouter() *chi.Mux {
+	corsAllowedOrigin := os.Getenv(config.EnvCorsAllowedOrigin)
+
+	router := chi.NewRouter()
+
+	router.Use(cors.Handler(cors.Options{
+		AllowedOrigins: []string{corsAllowedOrigin},
+		AllowedMethods: []string{"GET", "OPTIONS"},
+		AllowedHeaders: []string{"Accept", "Content-Type"},
+		MaxAge:         300,
+	}))
+
+	router.Get("/", HomeHandler)
+	router.Get("/strava-stats", StravaStatsHandler)
+
+	return router
+}
+
+func HomeHandler(res http.ResponseWriter, req *http.Request) {
+	res.Write([]byte("Hello World!"))
+}
+
+func StravaStatsHandler(res http.ResponseWriter, req *http.Request) {
+	stravaResponse, err := fetchStravaData()
+	if err != nil {
+		http.Error(res, fmt.Sprintf("Failed to fetch data from Strava: %v", err), http.StatusInternalServerError)
+		return
+	}
+
+	res.Header().Set("Content-Type", "application/json")
+
+	if err := json.NewEncoder(res).Encode(stravaResponse); err != nil {
+		http.Error(res, "Failed to encode JSON", http.StatusInternalServerError)
+	}
+}
+
+func fetchStravaData() (StravaStats, error) {
+	const cacheKey = "strava-stats"
+
+	if cachedData, found := stravaCache.Get(cacheKey); found {
+		return cachedData.(StravaStats), nil
+	}
+
+	athleteID := os.Getenv(config.EnvStravaAthleteID)
+	accessToken := os.Getenv(config.EnvStravaAccessToken)
+
+	stravaAPIURL := fmt.Sprintf("https://www.strava.com/api/v3/athletes/%s/stats", athleteID)
+
+	req, err := http.NewRequest("GET", stravaAPIURL, nil)
+	if err != nil {
+		return StravaStats{}, err
+	}
+
+	// Add the Authorization header
+	req.Header.Set("Authorization", "Bearer "+accessToken)
+
+	// Make the HTTP request
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return StravaStats{}, err
+	}
+	defer resp.Body.Close()
+
+	// Check if the response status is OK
+	if resp.StatusCode != http.StatusOK {
+		return StravaStats{}, fmt.Errorf("Strava API returned status: %s", resp.Status)
+	}
+
+	// Parse the response body
+	var stravaAPIResponse struct {
+		YtdRideTotals struct {
+			Count      int     `json:"count"`
+			Distance   int     `json:"distance"`
+			MovingTime float32 `json:"moving_time"`
+		} `json:"ytd_ride_totals"`
+		YtdRunTotals struct {
+			Count      int     `json:"count"`
+			Distance   int     `json:"distance"`
+			MovingTime float32 `json:"moving_time"`
+		} `json:"ytd_run_totals"`
+	}
+
+	if err := json.NewDecoder(resp.Body).Decode(&stravaAPIResponse); err != nil {
+		return StravaStats{}, err
+	}
+
+	stats := StravaStats{
+		Cycling: NewSportStats(
+			stravaAPIResponse.YtdRideTotals.Count,
+			int(stravaAPIResponse.YtdRideTotals.MovingTime),
+			stravaAPIResponse.YtdRideTotals.Distance,
+		),
+		Running: NewSportStats(
+			stravaAPIResponse.YtdRunTotals.Count,
+			int(stravaAPIResponse.YtdRunTotals.MovingTime),
+			stravaAPIResponse.YtdRunTotals.Distance,
+		),
+	}
+
+	stravaCache.Set(cacheKey, stats, cache.DefaultExpiration)
+
+	return stats, nil
+}
