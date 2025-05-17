3721eb9aa53b3983db40b24917f41116aae96c2e
Basti Buck <mail@bastibuck.de>
2025-05-17 22:21:54 +0200

    ensure FE env being correct

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,22 +1,30 @@
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-nocheck
+
 import path from "path";
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 import tailwindcss from "@tailwindcss/vite";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    TanStackRouterVite({
-      target: "react",
-    }),
-    react(),
-    tailwindcss(),
-  ],
+export default defineConfig(async ({ mode }) => {
+  import.meta.env = loadEnv(mode, process.cwd());
+  await import("./src/env");
 
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+  return {
+    plugins: [
+      TanStackRouterVite({
+        target: "react",
+      }),
+      react(),
+      tailwindcss(),
+    ],
+
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
     },
-  },
+  };
 });
