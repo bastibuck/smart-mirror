8d361eb6df3421ce1a51950e84479d7b0f0e9c78
Basti Buck <mail@bastibuck.de>
2025-06-07 11:48:22 +0200

    remove commented code

--- server/main.go
+++ server/main.go
@@ -7,16 +7,10 @@ import (
 	"smartmirror.server/env"
 	"smartmirror.server/router"
 	"smartmirror.server/shared"
-	// "github.com/twpayne/go-polyline"
 )
 
 func main() {
 	env.SetupEnv()
-
-	// polylineString := "_p~iF~ps|U_ulLnnqC_mqNvxq`@"
-	// buf := []byte(polylineString)
-	// coords, _, _ := polyline.DecodeCoords(buf)
-	// fmt.Println(coords)
 
 	router := router.SetupRouter()
 
