9b821bce5e152c768d024e4a69eae1db21235fe3
Basti Buck <mail@bastibuck.de>
2025-06-13 18:58:35 +0200

    show steps today

--- frontend/src/routes/(sports)/1.tsx
+++ frontend/src/routes/(sports)/1.tsx
@@ -2,6 +2,7 @@ import { createFileRoute } from "@tanstack/react-router";
 import AnnualStats from "@/widgets/strava/AnnualStats";
 import Clock from "@/widgets/clock/Clock";
 import LastActivity from "@/widgets/strava/LastActivity";
+import StepsToday from "@/widgets/garmin/StepsToday";
 
 export const Route = createFileRoute("/(sports)/1")({
   component: SportsPage,
@@ -11,6 +12,7 @@ function SportsPage() {
   return (
     <>
       <Clock position="top-left" />
+      <StepsToday position="top-right" />
       <LastActivity position="bottom-left" />
       <AnnualStats position="bottom-right" />
     </>
