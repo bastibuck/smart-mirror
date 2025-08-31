32ad8767c0648658852a7aba0f9622c64b418521
Basti Buck <mail@bastibuck.de>
2025-08-31 15:06:46 +0200

    add logging to grafana

--- server/utils/logging.go
+++ server/utils/logging.go
@@ -1,22 +1,48 @@
 package utils
 
 import (
+	"bytes"
 	"fmt"
 	"log"
+	"net/http"
+	"strconv"
 	"time"
+
+	"smartmirror.server/widgets/shared"
 )
 
 type Logger struct {
 	Info func(formattedString string, args ...interface{})
 }
 
-func NewLogger(widget string) Logger {
+var (
+	client = &http.Client{}
+)
 
+func NewLogger(widget string) Logger {
 	return Logger{
 		Info: func(formattedString string, args ...interface{}) {
-			timestamp := time.Now().Format("2006-01-02 15:04:05")
+			timestamp := time.Now()
+			logLine := fmt.Sprintf("[%s]: %s\n", widget, fmt.Sprintf(formattedString, args...))
 
-			log.Printf("[%s] %s: %s\n", widget, timestamp, fmt.Sprintf(formattedString, args...))
+			log.Print(logLine)
+
+			logs := fmt.Appendf(nil,
+				`{"streams": [{"stream": {"service_name": "Smartmirror backend", "widget": "%s", "level": "info"}, "values": [["%s", %q]]}]}`,
+				widget,
+				strconv.FormatInt(timestamp.UnixNano(), 10),
+				logLine,
+			)
+
+			req, err := http.NewRequest("POST", "https://logs-prod-012.grafana.net/loki/api/v1/push", bytes.NewBuffer(logs))
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+
+			req.Header.Set("Content-Type", "application/json")
+			req.SetBasicAuth("1320688", shared.GetGrafanaToken())
+			client.Do(req)
 		},
 	}
 }
--- server/widgets/shared/env.go
+++ server/widgets/shared/env.go
@@ -8,7 +8,8 @@ import (
 )
 
 const (
-	envAppMode = "APP_MODE" // "development" or "production"
+	envAppMode      = "APP_MODE" // "development" or "production"
+	envGrafanaToken = "GRAFANA_TOKEN"
 )
 
 var allowedModeValuesMap = map[string]bool{
@@ -22,8 +23,17 @@ func setDefaultEnv() {
 
 		env.SetDefaultValue(envAppMode, "production")
 	}
+
+	// Warn if GRAFANA_TOKEN is not set
+	if os.Getenv(envGrafanaToken) == "" {
+		fmt.Println("Warning: GRAFANA_TOKEN not set, logging to Grafana will fail")
+	}
 }
 
 func GetAppMode() string {
 	return os.Getenv(envAppMode)
+}
+
+func GetGrafanaToken() string {
+	return os.Getenv(envGrafanaToken)
 }
