a6236a5e597fadbd9b84b546e604318e0428b7ec
Basti Buck <mail@bastibuck.de>
2025-09-09 18:17:20 +0200

    remove buggy steps for now

--- frontend/src/routes/1/(sports)/index.tsx
+++ frontend/src/routes/1/(sports)/index.tsx
@@ -1,7 +1,6 @@
 import { createFileRoute } from "@tanstack/react-router";
 import AnnualStats from "@/widgets/strava/AnnualStats";
 import LastActivity from "@/widgets/strava/LastActivity";
-import StepsOfWeek from "@/widgets/garmin/StepsOfWeek";
 import Clock from "@/widgets/clock/Clock";
 
 export const Route = createFileRoute("/1/(sports)/")({
@@ -12,7 +11,7 @@ function SportsPage() {
   return (
     <>
       <Clock position="top-left" />
-      <StepsOfWeek position="top-right" />
+      {/* <StepsOfWeek position="top-right" /> */}
       <LastActivity position="bottom-left" />
       <AnnualStats position="bottom-right" />
     </>
