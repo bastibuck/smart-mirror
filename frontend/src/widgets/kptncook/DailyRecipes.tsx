65d3c72a24363dfe5cdc69e1c141ef957e78511f
Basti Buck <mail@bastibuck.de>
2025-10-11 18:28:08 +0200

    show more recipe infos

--- frontend/src/widgets/kptncook/DailyRecipes.tsx
+++ frontend/src/widgets/kptncook/DailyRecipes.tsx
@@ -1,18 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import WidgetPositioner from "../_layout/WidgetPositioner";
 import { useQuery } from "@tanstack/react-query";
 import { fetchUtil } from "@/lib/api";
 import { z } from "zod/v4";
+import { HeartIcon } from "lucide-react";
 
 const DailyRecipesSchema = z
   .object({
     title: z.string(),
+    favoriteCount: z.number().nonnegative().int(),
+    imageUrl: z.url(),
   })
   .array();
 
 const DailyRecipes: React.FC<React.ComponentProps<typeof WidgetPositioner>> = ({
   ...widgetPositionerProps
 }) => {
+  const [imageIndex, setImageIndex] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setImageIndex((prevIndex) => (prevIndex + 1) % 3); // Assuming there are 3 images
+    }, 10_000); // time of screen being displayed divided by number of images
+
+    return () => clearInterval(interval);
+  }, []);
+
   const { data, isPending, isError, error } = useQuery({
     queryKey: ["recipes", "daily"],
     queryFn: () => fetchUtil("/recipes/daily", DailyRecipesSchema),
@@ -30,16 +43,29 @@ const DailyRecipes: React.FC<React.ComponentProps<typeof WidgetPositioner>> = ({
     return <WidgetPositioner {...widgetPositionerProps} />;
   }
 
+  const recipe = data[imageIndex];
+
   return (
     <WidgetPositioner {...widgetPositionerProps}>
-      <div className="space-y-1">
-        <div className="text-3xl font-semibold">Kptn Cook</div>
+      <div className="space-y-2">
+        <figure className="relative inline-block">
+          <img src={recipe.imageUrl} alt={recipe.title} />
+          <div
+            className="absolute inset-0 flex h-full w-full items-end bg-gradient-to-b from-transparent via-transparent to-black"
+            key={recipe.title}
+          >
+            <div className="space-y-2">
+              <div className="text-2xl leading-tight text-shadow-black text-shadow-xs">
+                {recipe.title}
+              </div>
 
-        {data.map((recipe) => (
-          <div className="text-lg" key={recipe.title}>
-            {recipe.title}
+              <div className="flex items-center justify-end gap-2 font-mono text-base">
+                {Intl.NumberFormat().format(recipe.favoriteCount)}
+                <HeartIcon size="1em" />
+              </div>
+            </div>
           </div>
-        ))}
+        </figure>
       </div>
     </WidgetPositioner>
   );
