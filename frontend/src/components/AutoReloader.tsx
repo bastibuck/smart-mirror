eb485c177cd926d8d92a7067a7f205e500aeb9cc
Basti Buck <mail@bastibuck.de>
2025-11-09 18:15:28 +0100

    Log client-server version mismatch before reload

--- frontend/src/components/AutoReloader.tsx
+++ frontend/src/components/AutoReloader.tsx
@@ -17,6 +17,12 @@ const AutoReloader: React.FC = () => {
       );
 
       if (env.VITE_VERSION_HASH !== result.versionHash) {
+        // eslint-disable-next-line no-console
+        console.warn("Client-Server version mismatch found", {
+          client: env.VITE_VERSION_HASH,
+          server: result.versionHash,
+        });
+
         window.location.href = "/";
       }
 
