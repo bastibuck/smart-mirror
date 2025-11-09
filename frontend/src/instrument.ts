f1bd9eb3c98256ce2292b5af55baec46103c238d
Basti Buck <mail@bastibuck.de>
2025-11-09 16:24:09 +0100

    add source maps and logs

--- frontend/src/instrument.ts
+++ frontend/src/instrument.ts
@@ -7,5 +7,13 @@ if (env.VITE_IS_PROD && env.VITE_SENTRY_DSN !== "notset") {
     // Setting this option to true will send default PII data to Sentry.
     // For example, automatic IP address collection on events
     sendDefaultPii: true,
+
+    integrations: [
+      // send console.log, console.warn, and console.error calls as logs to Sentry
+      Sentry.consoleLoggingIntegration({ levels: ["log", "warn", "error"] }),
+    ],
+
+    // Enable logs to be sent to Sentry
+    enableLogs: true,
   });
 }
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,8 +4,9 @@
 import path from "path";
 import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
-import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
+import tanstackRouter from "@tanstack/router-plugin/vite";
 import tailwindcss from "@tailwindcss/vite";
+import { sentryVitePlugin } from "@sentry/vite-plugin";
 
 // https://vitejs.dev/config/
 export default defineConfig(async ({ mode }) => {
@@ -13,12 +14,22 @@ export default defineConfig(async ({ mode }) => {
   await import("./src/env");
 
   return {
+    build: {
+      sourcemap: true,
+    },
+
     plugins: [
-      TanStackRouterVite({
+      tanstackRouter({
         target: "react",
       }),
       react(),
       tailwindcss(),
+      sentryVitePlugin({
+        org: "bastibuck-org", // Your Sentry organization slug
+        project: "smartmirror", // Your Sentry project name
+        authToken:
+          "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",
+      }),
     ],
 
     resolve: {
