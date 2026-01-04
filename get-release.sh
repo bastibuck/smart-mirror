72de9bc492f4a40cd52ddb0ace4aaeb0fd2c44dc
Basti Buck <mail@bastibuck.de>
2026-01-04 20:54:09 +0100

    add checks

--- get-release.sh
+++ get-release.sh
@@ -1,5 +1,19 @@
-# !/bin/bash
+#!/bin/bash
 
 git fetch
+if [ $? -ne 0 ]; then
+    echo "Error: git fetch failed."
+    exit 1
+fi
+
 git rebase
+if [ $? -ne 0 ]; then
+    echo "Error: git rebase failed."
+    exit 1
+fi
+
 sudo make rebuild
+if [ $? -ne 0 ]; then
+    echo "Error: make rebuild failed."
+    exit 1
+fi
